2ea8936740b5f45a6c977459ab842c1890fc0025
luminukuJ <luminuku.pro@hotmail.com>
2022-03-04 16:28:22 +0100

    Add comment

--- app/src/components/CapsuleItem.vue
+++ app/src/components/CapsuleItem.vue
@@ -1,30 +1,33 @@
 <template>
+        <!-- This timeline item from Vuetify, Vuetify is a framework for Vue.js it's easier to implement a Vue with Vuetify -->
         <v-timeline-item
-          color="black"
+          color="blue"
           small
         >
           <v-row class="pt-1">
             <v-col cols="3">
+              <!-- We call the method capsuleDate to display the launch date -->
               <strong>{{capsuleDate}}</strong>
             </v-col>
             <v-col>
-
-              <div class="text-caption">
-                  <v-card-title class="text-h5"><strong>MISSION: {{capsule.missions}}</strong></v-card-title>
-                <v-card-text><strong>Capsule serie/type: </strong>{{capsule.capsule_serial}} / {{capsule.capsule_id}}</v-card-text>
+                <!-- Like the js syntax we can display a data with {} in the App.vue we use Axios to treat the get request with the liste of the capsule"    -->
+                <v-card-title class="text-h5"><b>Capsule:</b> {{capsule.capsule_serial}}</v-card-title>
+                <v-card-title class="text-h5"><b>Type:</b> {{capsule.capsule_id}}</v-card-title>
+                <v-card-title class="text-h5"><b>Status:</b> {{capsule.status}}</v-card-title>
               <v-card-text>{{capsule.details}}</v-card-text>
-              </div>
             </v-col>
           </v-row>
         </v-timeline-item>
 </template>
 
 <script> 
+// We use moment the for date format of the capsule launch date
 import moment from 'moment'
 export default{
     props: ["capsule"],
     computed: {
         capsuleDate() {
+          // We retrive the date of the capsule launch in American format and we modify it to display in European format
             return moment(this.capsule.original_launch).format("DD/MM/YYYY");
         }
     }
